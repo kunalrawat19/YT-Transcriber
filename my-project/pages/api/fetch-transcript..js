0856b1f3eb54aa09babb1ddcb9f14cdb36b56060
Devesh Tuli <deveshtuli02@gmail.com>
2024-10-02 22:59:49 +0530

    update file structure

--- my-project/pages/api/fetch-transcript..js
+++ my-project/pages/api/fetch-transcript..js
@@ -49,6 +49,7 @@ export default async function handler(req, res) {
     try {
       const transcript = await YoutubeTranscript.fetchTranscript(videoId);
       res.status(200).json(transcript);
+      console.log("hello")
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
